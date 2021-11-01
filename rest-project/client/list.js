d3d30a0cda0126828a28cfc1e3d84b1c930a9734
John Paxton (TJX) <tjx@speedingplanet.com>
2021-11-01 12:27:19 -0400

    Add hard-coded list.js

---
+++ rest-project/client/list.js
@@ -0,0 +1,22 @@
+import { data } from '../../data/students-module.js';
+
+let students = data.students;
+let list = document.querySelector('#list-body');
+let rows = generateRows(students);
+list.append(...rows);
+
+function generateRows(students) {
+  let rows = students.map((student) => {
+    let row = document.createElement('tr');
+    row.insertAdjacentHTML(
+      'beforeend',
+      `
+    <td>${student.firstName}</td>
+    <td>${student.lastName}</td>
+    <td>${student.location}</td>
+    `
+    );
+    return row;
+  });
+  return rows;
+}
