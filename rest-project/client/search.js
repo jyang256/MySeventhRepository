f274357c810e45b27cd1d393385005cea7f13279
John Paxton (TJX) <tjx@speedingplanet.com>
2021-11-02 00:28:44 -0400

    Reset part 2

--- rest-project/client/search.js
+++ rest-project/client/search.js
@@ -1,23 +1,5 @@
 let url = 'http://localhost:8000/students';
 
-document.querySelector('#search-button').addEventListener('click', () => {
-  let searchName = document.querySelector('#search-name').value;
-
-  axios
-    .get(url, {
-      params: {
-        lastName_like: searchName,
-      },
-    })
-    .then((response) => {
-      let students = response.data;
-      let list = document.querySelector('#search-results');
-      let rows = generateRows(students);
-      list.append(...rows);
-      list.parentElement.hidden = false;
-    });
-});
-
 function generateRows(students) {
   let rows = students.map((student) => {
     let row = document.createElement('tr');
